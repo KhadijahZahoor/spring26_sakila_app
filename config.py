88679136063ee4cfe263b072a8ff3e9d6c50c26b
KhadijahZahoor <f2023-956@bnu.edu.pk>
2026-04-26 18:58:12 +0500

    docs(config): clarify timeout environment variable

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
 MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
 MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
 
+# Timeout can be changed using the CONNECTION_TIMEOUT environment variable.
 CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
 HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
 
